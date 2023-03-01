6d3b172128e448ddba61cac0b98a80bbcaf239af
Milvus01 <cotcos.tansin@gmail.com>
2023-03-01 21:25:12 +0100

    test version

--- my_deep_learning.py
+++ my_deep_learning.py
@@ -1,24 +1,45 @@
+from time import time
 import matplotlib as plt
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import make_circles
 from tqdm import tqdm
-from math import e
 
 # par => parameters
 # act => activations
 # gra => gradiens
+# X => vecteur contenant les donnees
+# y => vecteur contenant la solution (donnees de reference)
 
-def initialisation(len):
-    a=0
-def forward_propagation(par):
-    a=0
+def initialisation(dim):
+    """Initialise un reseau de neuronnes :
+        (4, 6, 5)
+        -> 3 couches respectivement de 4, 6 et 5 neuronnes"""
+    par = {}
+    for c in range(1, len(dim)):
+        par[f'W{c}'] = np.random.randn(dim[c], dim[c - 1])
+        par[f'b{c}'] = np.random.randn(dim[c], 1)
+    return par
+def forward_propagation(X, par):
+    act ={'A0': X}
+    for c in range(1, len(par) // 2 + 1):
+        Z = par[f'W{c}'].dot(act[f'A{c - 1}']) + par[f'b{c}']
+        act[f'A{c}'] = 1 / (1 + np.exp(-Z)) # fonction sigmoide
+    return act
 def back_propagation(par, act):
-    a=0
+    gra = {}
+    dZ = act[f'A{len(par) // 2}'] - y
+    return gra
+def update(par, gra, learning_rate):
+    return par
 
+def neural_netowrk(X, y, n_iter=1000, learning_rate=0.1, hidden_layer=(32, 32, 32)):
+    len = list(hidden_layer)
+    len.insert(0, X.shape[0])
+    len.append(y.shape[0])
+    par = initialisation(len)
 
+    return par
 
+a = lambda x : 1 / (1 + 2.718281828459045**-x) # fonction sigmoide
 
-
-
-a = lambda x : 1 / (1 + e**-x) # fonction sigmoïde
